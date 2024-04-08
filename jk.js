36a4d77cd8054e1cb279591b5519bd553df4c084
extractg <98511780+extractg@users.noreply.github.com>
2024-04-08 20:11:13 +0300

    jk.js

--- jk.js
+++ jk.js
@@ -1,34 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const startBtn = document.getElementById('startBtn');
-    const stopBtn = document.getElementById('stopBtn');
+document.addEventListener('DOMContentLoaded', function () {
+    const runSpeedTestBtn = document.getElementById('run-speed-test');
 
-    // Event listener to start SpeedTest
-    startBtn.addEventListener('click', function() {
-        // Send request to start SpeedTest
-        fetch('/start_speedtest')
+    runSpeedTestBtn.addEventListener('click', function () {
+        fetch('/run_speed_test')
             .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
+                if (response.ok) {
+                    console.log('Speed test started.');
+                } else {
+                    console.error('Failed to start speed test.');
                 }
-                console.log('SpeedTest started successfully');
             })
-            .catch(error => {
-                console.error('There was a problem with starting SpeedTest:', error);
-            });
-    });
-
-    // Event listener to stop SpeedTest
-    stopBtn.addEventListener('click', function() {
-        // Send request to stop SpeedTest
-        fetch('/stop_speedtest')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                console.log('SpeedTest stopped successfully');
-            })
-            .catch(error => {
-                console.error('There was a problem with stopping SpeedTest:', error);
-            });
+            .catch(error => console.error('Error occurred:', error));
     });
 });
